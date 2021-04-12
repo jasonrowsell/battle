8d1479b17f76522936de2b1840f10deddd010ea7
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-12 23:51:35 +0100

    Updated feature test

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,6 +4,7 @@ feature 'Enter names' do
     fill_in :player_1_name, with: 'Player 1'
     fill_in :player_2_name, with: 'Player 2'
     click_button 'Submit'
+    save_and_open_page
     expect(page).to have_content 'Player 1 vs. Player 2'
   end
 end
